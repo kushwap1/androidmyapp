2630d7dc1c61162d10c0e0db69f98e201e770c8c
kushwap1 <pankaj.kushwaha@yahoo.com>
2023-03-14 01:16:12 +0530

    first

---
+++ app/src/main/java/shop/pankajk/myapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package shop.pankajk.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
